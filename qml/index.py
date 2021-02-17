a4392cf13b8b37df955fc8459784ab422cd5f3fd
soongaya <sammiongaya@gmail.com>
2021-02-17 20:42:34 +0300

    Added app structure

--- qml/index.py
+++ qml/index.py
@@ -1,10 +1,7 @@
-from flask import Flask  
-  
-app = Flask(__name__)  
- 
-@app.route('/')    
-def home():  
-    return "Helo Dev";  
-  
-if __name__ =='__main__':  
-    app.run(debug = True)  
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
