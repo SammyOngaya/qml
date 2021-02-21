0fd90091e86d0bbfd8d87ded97c3a1dc8a529ded
soongaya <sammiongaya@gmail.com>
2021-02-21 17:54:06 +0300

    The app is able to return the uploaded file successfully

--- qml/apps/explore.py
+++ qml/apps/explore.py
@@ -6,14 +6,18 @@ import dash_uploader as du
 from dash.dependencies import Input, Output,State
 import pandas as pd
 import pathlib
+import uuid
+import glob
+import os
 
 from app import app, server
 
 
 PATH=pathlib.Path(__file__).parent
 DATA_PATH=PATH.joinpath("../datasets").resolve()
+du.configure_upload(app, DATA_PATH, use_upload_id=False)
 
-du.configure_upload(app, DATA_PATH)
+
 
 layout=dbc.Container([
 
@@ -28,7 +32,8 @@ layout=dbc.Container([
     brand_href="/apps/home",
     color="primary",
     dark=True,
-    style={'margin-bottom': '2px'}
+    style={'margin-bottom': '2px'},
+
 
 ),#end navigation
 
@@ -42,34 +47,43 @@ layout=dbc.Container([
     #1.
         dbc.Row(
             [
-                dbc.Col(html.Div([
-                  # html.H6("Analyse your data"),
+                dbc.Col(html.Div([                  
+                   du.Upload( id='dash-uploader',
+                        max_file_size=2,  # 1800 Mb
+                        filetypes=['csv', 'zip'],
+                        # upload_id=uuid.uuid1(),  # Unique session id
+                        text='Drag and Drop a File Here to upload!',
+                        text_completed='File Sucessfully Uploaded: ',
+                           ),
                   ] 
                 	),
 			style={
             'margin-top': '30px'
             },
-                	md=0),
+                	md=4),
    #2.
                       dbc.Col(html.Div([
-                    html.H6("Explore your Raw Data") , 
-                    du.Upload(),
+                    # html.H6("# Records :") , 
+                    html.Div(id='callback-output'),
+                   
+                    
                     ]
                   ),
       style={
             'margin-top': '30px'
             },
-                  md=12),
+                  md=4),
    #3. doughnut_pie_chart_with_center
                        dbc.Col(html.Div(
               [
                 # html.H6("Tweet Analysis") , 
+                # html.Div(id='callback-output'),
                    ]
                   ),
       style={
             'margin-top': '30px'
             },
-                  md=0),
+                  md=4),
 
             ]
         ),
@@ -122,3 +136,17 @@ layout=dbc.Container([
 	],
 	fluid=True
 	)
+
+
+
+@app.callback(
+    Output('callback-output', 'children'),
+    [Input('dash-uploader', 'isCompleted')],
+    [State('dash-uploader', 'fileNames'),
+     State('dash-uploader', 'upload_id')],
+)
+def callback_on_completion(iscompleted, filenames, upload_id):
+  data=[str(x) for x in filenames]
+  file=str(data).replace("['","").replace("']","")
+  df=pd.read_csv(DATA_PATH.joinpath(file))
+  return "# Records : "+str(df.shape[0])+" | # Attributes : "+str(df.shape[1])
